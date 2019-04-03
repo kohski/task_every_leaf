74f1e569f319e9ae6e0daf5de1a16b08254ddb4e
木村　幸介 <kosukekimura0528@gmail.com>
2019-04-03 11:30:33 +0900

    【step13】過去修正の反映/BASIC認証の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic
-  Bundler.require(*Rails.groups)
-  Dotenv::Railtie.load
-
-  private
-  def basic
-    authenticate_or_request_with_http_basic do |name, password|
-      name == ENV['BASIC_NAME'] && password == ENV['BASIC_PASSWORD']
-    end
-  end
 end
