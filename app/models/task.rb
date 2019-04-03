e0d146c75e4c2d285a403d08feea2c2aeada575c
木村　幸介 <kosukekimura0528@gmail.com>
2019-04-03 13:29:39 +0900

    【step15】status columnの追加と、view修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length:{ in: 1..255  }
   validates :content, length:{ maximum: 1000  }
+  validates :status, numericality: { only_integer: true , greater_than_or_equal_to: 0,less_than_or_equal_to:2 }
 end
