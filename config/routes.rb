f5f96ada092c2f807052d27c7ec648128497e8b8
木村　幸介 <kosukekimura0528@gmail.com>
2019-04-04 16:22:46 +0900

    【step19】ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
-  resources :users,only:[:show,:new,:create,:edit,:update,:destroy]
+  resources :users, only:[:show, :new, :create, :edit, :update, :destroy]
+  resources :sessions, only:[:new, :create,:destroy]
 end
